e506f0c883da69f5501fc5cc4fd0d74ef5766aff
IDGS-803-20000810 <jalexmr02@gmail.com>
2024-01-23 18:42:49 -0600

    Operaciones basicas 23/01/2024

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("main.html")
+
+@app.route("/formOpBas")
+def formOpBas():
+    return render_template("formularioOpBas.html")
+
+@app.route("/resOpBas",methods=["GET","POST"])
+def resOpBas():
+    if request.method == "POST":
+        num1=request.form.get("n1")
+        num2=request.form.get("n2")
+        operacion=request.form.get("radioOp")
+        
+        if str(operacion)=="Suma":
+            res = int(num1)+int(num2)
+        elif str(operacion)=="Resta":
+            res = int(num1)-int(num2)
+        elif str(operacion)=="Multiplicacion":
+            res = int(num1)*int(num2)
+        elif str(operacion)=="Division":
+            res = int(num1)/int(num2)
+            
+        
+        return "<h1>El resultado es {}</h1>".format(str(res))
+        
+        # return "<h1>opreacion es {}</h1>".format(str(int(num1)*int(num2)))
+    return 
+
+
+if __name__=="__main__":
+    app.run(debug=True)
